9384542b4071e841ef96d419c3d92a16a9fcd181
matthowe <mhowe@headingtonschool.com>
2017-02-06 15:59:41 +0000

    inefficient but working

--- permutations.py
+++ permutations.py
@@ -13,32 +13,32 @@ freeSquares = freeSpaces(board)
 
 for y in range(8):
     for x in range(8):
+        print (y + x)
         board = createBoard()
-        queens = 0
+        queens = 1
         placeQueen(board,y,x)
         freeSquares = freeSpaces(board)
 
         running = 0
-        print("running permutations - do not touch")
+        
         for p in permutations(freeSquares):
-            #running = running  + 1
-            #print("permutation running ", running)
-            
+            print(p)
             for i in p:
                 #print (i[0],i[1])
                 if placeQueen(board,i[0],i[1]) == True:
                     queens = queens + 1
                     #print(queens)
-                if queens > 4:
+                if queens > 7:
                     displayBoard(board)
                     board = createBoard()
                     placeQueen(board,y,x)
-                    queens = 0
+                    queens = 1
+                    next
                 if isBoardFull(board) == True:
                     board = createBoard()
                     placeQueen(board,y,x)
-                    queens = 0
-                    break
+                    queens = 1
+                    next 
                 
             
         
